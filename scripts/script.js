3205a1807cf7566bedb32734e0318871be997593
JohnnyCaringi <92838873+JohnnyCaringi@users.noreply.github.com>
2023-09-14 14:36:22 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,22 @@
+/*
+Name: spiritFinder
+Input: Someone's name
+Output: A "Spirit Number"
+Description: Does math involving a name to give a "Spirit Number"
+             which can later be used to find a Spirit Animal.
+*/
+function spiritFinder(name){
+    spiritNumber = (name.length * name.charCodeAt(0)) % 5;
+
+    return spiritNumber;
+}
+
 $( document ).ready(function() {
 
     $("#submit").click(function(){
         let name = $('#name').val()
-        /*
-        Takes the length, first letter, and last letter of a name for
-        a unique enough value. Then we modulus it by 5 so this number
-        determines which of the 5 animals you are
-        */
-        let animal = (name.length * name.charCodeAt(0) * name.charCodeAt(name.length-1)) % 5;
+
+        let animal = spiritFinder(name);
         alert(animal);
     });
 
